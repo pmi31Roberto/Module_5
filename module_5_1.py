050d4e285fd77ea3f83e76717520bf9d2c1fbae0
Роберт <dificalabc@gmail.com>
2024-08-30 23:32:34 +0500

    второе дз модуль 5

--- module_5_1.py
+++ module_5_1.py
@@ -3,6 +3,12 @@ class House:
         self.name = name
         self.number_of_floors = number
 
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
             print("Такого этажа не существует")
@@ -11,7 +17,3 @@ class House:
                 print(i)
 
 
-h1 = House('ЖК Горский', 18)
-h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
---
+++ module_5_2.py
@@ -0,0 +1,12 @@
+from module_5_1 import *
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
