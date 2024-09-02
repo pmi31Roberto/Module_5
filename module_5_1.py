772237ebef1bfb2e620b2fea1705b12e2e8e45bd
Роберт <dificalabc@gmail.com>
2024-09-02 23:07:51 +0500

    3 дз модуля 5

--- module_5_1.py
+++ module_5_1.py
@@ -1,13 +1,41 @@
 class House:
-    def __init__(self, name, number):  # Инициализация
+    def __init__(self, name, number_of_floors):  # Инициализация
         self.name = name
-        self.number_of_floors = number
+        self.number_of_floors = number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __len__(self):
         return self.number_of_floors
 
-    def __str__(self):
-        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
 
     def go_to(self, new_floor):
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -15,5 +43,3 @@ class House:
         else:
             for i in range(1, new_floor + 1):
                 print(i)
-
-
--- module_5_2.py
+++ module_5_2.py
@@ -6,7 +6,6 @@ h2 = House('ЖК Акация', 20)
 # __str__
 print(h1)
 print(h2)
-
 # __len__
 print(len(h1))
 print(len(h2))
